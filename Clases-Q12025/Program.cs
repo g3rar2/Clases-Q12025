4e6d0833157d9f762a6422eaa98a157dd8104103
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-22 18:57:16 -0600

    Adding ave data

--- Clases-Q12025/Program.cs
+++ Clases-Q12025/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Clases_Q12025;
+
+Ave ave = new Ave();
+ave.Nombre = "Loro";
+ave.Especie = "Amazona eastiva";
+ave.Color = "Verde";
+ave.Tamano = 30.5;
+ave.Habitat = "Bosques Tropicales";
+ave.Imprimir();
+
