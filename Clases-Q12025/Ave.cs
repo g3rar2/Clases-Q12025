9e28f46573e2616eebb7eb35be4bca89913718b2
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-22 18:57:02 -0600

    Create Ave Class

---
+++ Clases-Q12025/Ave.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Clases_Q12025
+{
+    public class Ave
+    {
+        public string? Nombre { get; set; }
+        public string? Especie { get; set; }
+        public string? Color { get; set; }
+        public double Tamano { get; set; }
+
+        public string? Habitat { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Especie: {Especie}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamano: {Tamano}");
+            Console.WriteLine($"Habitat: {Habitat}");
+
+
+        }
+
+
+
+    }
+}
